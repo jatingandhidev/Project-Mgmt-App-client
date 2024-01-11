f942de193fdf7383d67e19661b3d1a5643cc8e2a
Salvad0r14 <jatin05092001@gmail.com>
2024-01-11 16:44:37 +0530

    url updated

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const cache = new InMemoryCache({
 })
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://project-mgmt-app-server.onrender.com/graphql',
   cache,
 })
 
